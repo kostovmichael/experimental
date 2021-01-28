7ee5820cb33d94acbc4241b1fecc4393331bd27a
Mike Kostov <mike.kostov@progleasing.com>
2021-01-28 16:48:28 -0700

    adding algs related classes

---
+++ javasamples/AlgsSandbox/src/com/algexperimental/UnionFindFactory.java
@@ -0,0 +1,18 @@
+package com.algexperimental;
+
+public class UnionFindFactory {
+
+    public void BuildWeightedQuickUnionUF(String url){
+        new In(url);
+        int n = StdIn.readInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.find(p) == uf.find(q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+}

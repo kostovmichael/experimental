980b4232c582922db7409166943623887490be2a
Mike Kostov <mike.kostov@progleasing.com>
2021-02-02 19:27:53 -0700

    thread safe example of singleton implementation

---
+++ samples/PerformanceConcepts/Patterns/Creational/ThreadSafeSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PatternsAndConcepts.Patterns.Creational
+{
+    /// <summary>
+    /// Allows only a single thread to enter the critical area,
+    /// which the lock block identifies, when no instance of
+    /// ThreadSafeSingleton has yet been created
+    /// For more infor please see https://docs.microsoft.com/en-us/previous-versions/msp-n-p/ff650316(v=pandp.10)?redirectedfrom=MSDN#multithreaded-singleton
+    /// </summary>
+    public sealed class ThreadSafeSingleton
+    {
+        private static volatile ThreadSafeSingleton instance;
+        private static object syncRoot = new Object();
+
+        private ThreadSafeSingleton() { }
+
+        public static ThreadSafeSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new ThreadSafeSingleton();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}

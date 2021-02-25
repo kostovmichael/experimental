3da6e81d6f99aef3c83f97398e4584a82147e813
Mike Kostov <mike.kostov@progleasing.com>
2021-02-24 23:07:08 -0700

    minor updates

--- samples/BenchmarkNetPlayground/Configs/ManualConfigurations.cs
+++ samples/BenchmarkNetPlayground/Configs/ManualConfigurations.cs
@@ -2,143 +2,137 @@
 
 namespace BenchmarkNetPlayground.Configs
 {
-   using BenchmarkDotNet.Columns;
-   using BenchmarkDotNet.Configs;
-   using BenchmarkDotNet.Diagnosers;
-   using BenchmarkDotNet.Environments;
-   using BenchmarkDotNet.Exporters;
-   using BenchmarkDotNet.Jobs;
-   using BenchmarkDotNet.Loggers;
+    using BenchmarkDotNet.Columns;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Environments;
+    using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Loggers;
 
-   public static class ManualConfigurations
-   {
-      public const string ArtifactsPath = @"C:\ProgAppLogs\BenchmarkDotNet";
+    public static class ManualConfigurations
+    {
+        public const string ArtifactsPath = @"C:\ProgAppLogs\BenchmarkDotNet";
 
-      #region "Manual Configs"
-      public static ManualConfig GetManualConfigDefault()
-      {
-         ManualConfig config = ManualConfig.CreateEmpty();
-         //https://benchmarkdotnet.org/articles/configs/diagnosers.html
-         config.AddDiagnoser(MemoryDiagnoser.Default);
-         config.WithArtifactsPath(ArtifactsPath);
-         config.AddExporter(DefaultExporters.Html);
-         config.AddLogger(ConsoleLogger.Default);
-         config.AddColumnProvider(DefaultColumnProviders.Instance);
-         return config;
+        #region "Manual Configs"
+        public static ManualConfig GetManualConfigDefault()
+        {
+            ManualConfig config = ManualConfig.CreateEmpty();
+            //https://benchmarkdotnet.org/articles/configs/diagnosers.html
+            config.AddDiagnoser(MemoryDiagnoser.Default);
+            config.WithArtifactsPath(ArtifactsPath);
+            config.AddExporter(DefaultExporters.Html);
+            config.AddLogger(ConsoleLogger.Default);
+            config.AddColumnProvider(DefaultColumnProviders.Instance);
+            return config;
 
-      }
-      public static ManualConfig GetManualConfig_Default_Job()
-      {
-         ManualConfig config = GetManualConfigDefault();
-         config.AddJob(Job.Default.WithGcServer(true));
-         return config;
+        }
+        public static ManualConfig GetManualConfig_Default_Job()
+        {
+            ManualConfig config = GetManualConfigDefault();
+            config.AddJob(Job.Default.WithGcServer(true));
+            return config;
 
-      }
-
-
+        }
 
 
-      public static ManualConfig GetManualConfig_Net472_CoreRt31_X86()
-      {
-         ManualConfig config = GetManualConfigDefault();
-         config.AddJob(Get_Job_DotNet_Legacy_472(runAsX64: false).AsBaseline(), Get_Job_DotNet_Core_31(runAsX64: false));
 
-         return config;
-      }
 
-      public static ManualConfig GetManualConfig_Net472_CoreRt31_64()
-      {
-         ManualConfig config = GetManualConfigDefault();
-         config.AddJob(Get_Job_DotNet_Legacy_472().AsBaseline(), Get_Job_DotNet_Core_31());
-         return config;
-      }
+        public static ManualConfig GetManualConfig_Net472_CoreRt31_X86()
+        {
+            ManualConfig config = GetManualConfigDefault();
+            config.AddJob(Get_Job_DotNet_Legacy_472(runAsX64: false).AsBaseline(), Get_Job_DotNet_Core_31(runAsX64: false));
 
-      public static ManualConfig GetManualConfig_Net472_CoreRt31_DotNet50_64()
-      {
-         ManualConfig config = GetManualConfigDefault();
-         config.AddJob(Get_Job_DotNet_Legacy_472());
-         config.AddJob(Get_Job_DotNet_Core_31());
-         config.AddJob(Get_Job_DotNet_50());
-         return config;
-      }
+            return config;
+        }
 
-      public static ManualConfig GetManualConfig_Net472_CoreRt31_DotNet50_64_backup()
-      {
-         ManualConfig config = GetManualConfigDefault();
-         config.AddJob(Job.ShortRun
-               .WithRuntime(ClrRuntime.Net472)
+        public static ManualConfig GetManualConfig_Net472_CoreRt31_64()
+        {
+            ManualConfig config = GetManualConfigDefault();
+            config.AddJob(Get_Job_DotNet_Legacy_472().AsBaseline(), Get_Job_DotNet_Core_31());
+            return config;
+        }
+
+        public static ManualConfig GetManualConfig_Net472_CoreRt31_DotNet50_64()
+        {
+            ManualConfig config = GetManualConfigDefault();
+            config.AddJob(Get_Job_DotNet_Legacy_472());
+            config.AddJob(Get_Job_DotNet_Core_31());
+            config.AddJob(Get_Job_DotNet_50());
+            return config;
+        }
+
+        public static ManualConfig GetManualConfig_Net472_CoreRt31_DotNet50_64_backup()
+        {
+            ManualConfig config = GetManualConfigDefault();
+            config.AddJob(Job.Default
+                  .WithRuntime(ClrRuntime.Net472)
+                  .WithPlatform(Platform.X64)
+                  .WithGcServer(true)
+                  .WithId("Net472")
+
+            );
+
+            config.AddJob(Job.Default
+               .WithRuntime(CoreRuntime.Core31)
                .WithPlatform(Platform.X64)
-               .WithJit(Jit.RyuJit)
                .WithGcServer(true)
-               //.WithBaseline(true)
-               //.AsBaseline()
-               .WithId("Net472")
+               .WithId("CoreRt31")
 
-         );
+            );
 
-         config.AddJob(Job.ShortRun
-            .WithRuntime(CoreRtRuntime.CoreRt31)
-            .WithPlatform(Platform.X64)
-            .WithJit(Jit.RyuJit)
-            .WithGcServer(true)
-            .WithId("CoreRt31")
+            config.AddJob(Job.Default
+                  .WithRuntime(CoreRuntime.Core50)
+                  .WithPlatform(Platform.X64)
+                  .WithGcServer(true)
+                  .WithId("CoreRt50")
+            );
 
-         );
+            return config;
 
-         //config.AddJob(Job.ShortRun
-         //      .WithRuntime(CoreRuntime.Core50)
-         //      .WithPlatform(Platform.X64)
-         //      .WithJit(Jit.RyuJit)
-         //      .WithGcServer(true)
-         //      .WithId("CoreRt50")
-         ////.WithMaxIterationCount(maxIterationsCount)// Max Iterations (Default is 100)
-         //);
-
-         return config;
-
-      }
+        }
 
 
-      #endregion "Manual Configs"
+        #endregion "Manual Configs"
 
 
-      #region "Jobs"
+        #region "Jobs"
 
-      public static Job Get_Job_DotNet_Legacy_472(bool runAsX64 = true)
-      {
-         var job = new Job(Job.Default);
-         job.WithId("Net472");
-         job.WithRuntime(ClrRuntime.Net472);
-         job.WithPlatform(runAsX64 ? Platform.X64 : Platform.X86);
-         job.WithJit(Jit.LegacyJit);
-         job.WithGcServer(true);
-         return job;
-      }
+        public static Job Get_Job_DotNet_Legacy_472(bool runAsX64 = true)
+        {
+            var job = new Job(Job.Default);
+            job.WithId("Net472");
+            job.WithRuntime(ClrRuntime.Net472);
+            job.WithPlatform(runAsX64 ? Platform.X64 : Platform.X86);
+            job.WithJit(Jit.LegacyJit);
+            job.WithGcServer(true);
+            return job;
+        }
 
-      public static Job Get_Job_DotNet_Core_31(bool runAsX64 = true)
-      {
+        public static Job Get_Job_DotNet_Core_31(bool runAsX64 = true)
+        {
 
-         var job = new Job(Job.Default);
-         job.WithId("CoreRt31");
-         job.WithRuntime(CoreRtRuntime.CoreRt31);
-         job.WithPlatform(runAsX64 ? Platform.X64 : Platform.X86);
-         job.WithJit(Jit.RyuJit);
-         job.WithGcServer(true);
-         return job;
-      }
+            var job = new Job(Job.Default);
+            job.WithId("CoreRt31");
+            job.WithRuntime(CoreRtRuntime.CoreRt31);
+            job.WithPlatform(runAsX64 ? Platform.X64 : Platform.X86);
+            job.WithJit(Jit.RyuJit);
+            job.WithGcServer(true);
+            return job;
+        }
 
-      public static Job Get_Job_DotNet_50(bool runAsX64 = true)
-      {
-         var job = new Job(Job.Default);
-         job.WithId("CoreRt50");
-         job.WithRuntime(CoreRtRuntime.CoreRt50);
-         job.WithPlatform(runAsX64 ? Platform.X64 : Platform.X86);
-         job.WithJit(Jit.RyuJit);
-         job.WithGcServer(true);
-         return job;
-      }
+        public static Job Get_Job_DotNet_50(bool runAsX64 = true)
+        {
+            var job = new Job(Job.Default);
+            job.WithId("CoreRt50");
+            job.WithRuntime(CoreRtRuntime.CoreRt50);
+            job.WithPlatform(runAsX64 ? Platform.X64 : Platform.X86);
+            job.WithJit(Jit.RyuJit);
+            job.WithGcServer(true);
+            return job;
+        }
 
-      #endregion "Jobs"
+        #endregion "Jobs"
 
-   }
+    }
 }
--- samples/BenchmarkNetPlayground/Optimizations/Loops/ForLoopTesting.cs
+++ samples/BenchmarkNetPlayground/Optimizations/Loops/ForLoopTesting.cs
@@ -1,155 +1,180 @@
 ﻿namespace BenchmarkNetPlayground.Optimizations.Loops
 {
-   using BenchmarkDotNet.Attributes;
-   using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Analysers;
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Columns;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Environments;
+    using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Validators;
 
-   using DummyClasses;
+    using DummyClasses;
 
-   using Services;
+    using Perfolizer.Horology;
 
-   using System.Collections.Generic;
+    using Services;
 
-   using Utils;
+    using System.Collections.Generic;
+    using System.Linq;
 
-   public class ForLoopTesting
-   {
+    using Utils;
 
-      //[SimpleJob(RuntimeMoniker.Net472)]
-      ////https://benchmarkdotnet.org/articles/configs/diagnosers.html
-      //[MemoryDiagnoser]
-      ////[ArtifactsPath(@"C:\ProgAppLogs")]
-      //[HtmlExporter]
-      public class ForVsForEach
-      {
-
-         private List<Product> listOfProducts;
-         private string[] arrayOfStrings;
-
-         [GlobalSetup]
-         public void GlobalSetup()
-         {
-            arrayOfStrings = TestDataRetrievalService.GetMThesaurStringArray();
-            int upperBound = arrayOfStrings.Length;
-            listOfProducts = new List<Product>(upperBound);
-            for (int i = 0; i < upperBound; i++)
+    public class ForLoopTesting
+    {
+        [Config(typeof(Config))]
+        public class ForVsForEach
+        {
+            private class Config : ManualConfig
             {
-               listOfProducts.Add(new Product()
-               {
-                  Id = i + 100,
-                  Name = arrayOfStrings[i]
-               });
-            }
-
-         }
-
-         [Benchmark]
-         public Dictionary<int, Product> ForLoop()
-         {
-            int upperBound = listOfProducts.Count;
-            var dict = new Dictionary<int, Product>(listOfProducts.Count);
-            for (int i = 0; i < upperBound; i++)
-            {
-               var product = listOfProducts[i];
-               dict.Add(product.Id, product);
-            }
-            return dict;
-         }
-
-         [Benchmark]
-         public Dictionary<int, Product> ForEach()
-         {
-
-            var dict = new Dictionary<int, Product>(listOfProducts.Count);
-            foreach (var product in listOfProducts)
-            {
-               dict.Add(product.Id, product);
-            }
-            return dict;
-         }
-
-
-         [Benchmark]
-         public Dictionary<int, Product> ForEachWithLambda()
-         {
-            var dict = new Dictionary<int, Product>(listOfProducts.Count);
-            listOfProducts.ForEach(product =>
+                public Config()
                 {
-                   dict.Add(product.Id, product);
+                    // The same, using the .With() factory methods:
+                    AddJob(
+                        Job.ShortRun
+                            .WithPlatform(Platform.X64)
+                            .WithJit(Jit.RyuJit)
+                            .WithRuntime(CoreRuntime.Core50)
+                            .WithMaxRelativeError(0.01)
+                            .WithId("Core50Job")
+                    );
+                    AddDiagnoser(MemoryDiagnoser.Default);
+                    // Exporters for data
+                    AddExporter(GetExporters().ToArray());
+                    AddColumn(StatisticColumn.AllStatistics);
+
+                }
+            }
+
+            private List<Product> listOfProducts;
+            private string[] arrayOfStrings;
+
+            [GlobalSetup]
+            public void GlobalSetup()
+            {
+                arrayOfStrings = TestDataRetrievalService.GetMThesaurStringArray();
+                int upperBound = arrayOfStrings.Length;
+                listOfProducts = new List<Product>(upperBound);
+                for (int i = 0; i < upperBound; i++)
+                {
+                    listOfProducts.Add(new Product()
+                    {
+                        Id = i + 100,
+                        Name = arrayOfStrings[i]
+                    });
+                }
+
+            }
+
+            [Benchmark]
+            public Dictionary<int, Product> ForLoop()
+            {
+                int upperBound = listOfProducts.Count;
+                var dict = new Dictionary<int, Product>(listOfProducts.Count);
+                for (int i = 0; i < upperBound; i++)
+                {
+                    var product = listOfProducts[i];
+                    dict.Add(product.Id, product);
+                }
+                return dict;
+            }
+
+            [Benchmark]
+            public Dictionary<int, Product> ForEach()
+            {
+
+                var dict = new Dictionary<int, Product>(listOfProducts.Count);
+                foreach (var product in listOfProducts)
+                {
+                    dict.Add(product.Id, product);
+                }
+                return dict;
+            }
+
+
+            [Benchmark]
+            public Dictionary<int, Product> ForEachWithLambda()
+            {
+                var dict = new Dictionary<int, Product>(listOfProducts.Count);
+                listOfProducts.ForEach(product =>
+                    {
+                        dict.Add(product.Id, product);
+                    });
+
+                return dict;
+            }
+
+
+
+        }
+
+        public class ForVsForEachSlim
+        {
+
+            private List<Product> listOfProducts;
+            private string[] arrayOfStrings;
+
+            [GlobalSetup]
+            public void GlobalSetup()
+            {
+                arrayOfStrings = TestDataRetrievalService.GetMThesaurStringArray();
+                int upperBound = arrayOfStrings.Length;
+                listOfProducts = new List<Product>(upperBound);
+                for (int i = 0; i < upperBound; i++)
+                {
+                    listOfProducts.Add(new Product()
+                    {
+                        Id = i + 100,
+                        Name = arrayOfStrings[i]
+                    });
+                }
+
+            }
+
+            [Benchmark]
+            public long ForLoop()
+            {
+                long result = 0;
+                int upperBound = listOfProducts.Count;
+                for (int i = 0; i < upperBound; i++)
+                {
+                    var product = listOfProducts[i];
+                    result += product.Id;
+                }
+                return result;
+            }
+
+            [Benchmark]
+            public long ForEach()
+            {
+
+                long result = 0;
+                foreach (var product in listOfProducts)
+                {
+                    result += product.Id;
+                }
+                return result;
+            }
+
+
+            [Benchmark]
+            public long ForEachWithLambda()
+            {
+                long result = 0;
+                listOfProducts.ForEach(product =>
+                {
+                    result += product.Id;
                 });
 
-            return dict;
-         }
-
-
-
-      }
-
-      public class ForVsForEachSlim
-      {
-
-         private List<Product> listOfProducts;
-         private string[] arrayOfStrings;
-
-         [GlobalSetup]
-         public void GlobalSetup()
-         {
-            arrayOfStrings = TestDataRetrievalService.GetMThesaurStringArray();
-            int upperBound = arrayOfStrings.Length;
-            listOfProducts = new List<Product>(upperBound);
-            for (int i = 0; i < upperBound; i++)
-            {
-               listOfProducts.Add(new Product()
-               {
-                  Id = i + 100,
-                  Name = arrayOfStrings[i]
-               });
+                return result;
             }
 
-         }
-
-         [Benchmark]
-         public long ForLoop()
-         {
-            long result = 0;
-            int upperBound = listOfProducts.Count;
-            for (int i = 0; i < upperBound; i++)
-            {
-               var product = listOfProducts[i];
-               result += product.Id;
-            }
-            return result;
-         }
-
-         [Benchmark]
-         public long ForEach()
-         {
-
-            long result = 0;
-            foreach (var product in listOfProducts)
-            {
-               result += product.Id;
-            }
-            return result;
-         }
 
 
-         [Benchmark]
-         public long ForEachWithLambda()
-         {
-            long result = 0;
-            listOfProducts.ForEach(product =>
-            {
-               result += product.Id;
-            });
+        }
 
-            return result;
-         }
-
-
-
-      }
-
-   }
+    }
 
 
 }

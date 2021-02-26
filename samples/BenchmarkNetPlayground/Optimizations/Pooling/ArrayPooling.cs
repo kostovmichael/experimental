2a8fd3f3987a8c44e9156b60d3be734f0ea6dbd2
Mike Kostov <mike.kostov@progleasing.com>
2021-02-26 11:41:00 -0700

    array pooling

--- samples/BenchmarkNetPlayground/Optimizations/Pooling/ArrayPooling.cs
+++ samples/BenchmarkNetPlayground/Optimizations/Pooling/ArrayPooling.cs
@@ -1,6 +1,9 @@
 ﻿namespace BenchmarkNetPlayground.Optimizations.Pooling
 {
     using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Engines;
+    using BenchmarkDotNet.Jobs;
 
     using PatternsAndConcepts.DummyModels;
     using PatternsAndConcepts.Services;
@@ -9,60 +12,117 @@
 
     public class ArrayPooling
     {
-        private string[] arrayOfStrings;
-
-        [GlobalSetup]
-        public void GlobalSetup()
+        [MemoryDiagnoser]
+        [Config(typeof(DontForceGcCollectionsConfig))] // we don't want to interfere with GC, we want to include it's impact
+        public class ArrayPoolOfStructs
         {
-            arrayOfStrings = TestDataRetrievalService.GetMThesaurStringArray();
-        }
+            private string[] arrayOfStrings;
 
-        [Benchmark]
-        public long ArrayWithoutPool()
-        {
-            int upperBound = arrayOfStrings.Length;
-            ProductStruct[] data = new ProductStruct[upperBound];
-            long result = 0;
-            for (int i = 0; i < upperBound; ++i)
+            [GlobalSetup]
+            public void GlobalSetup()
             {
-                data[i] = new ProductStruct()
+                arrayOfStrings = TestDataRetrievalService.GetMThesaurStringArray();
+            }
+
+            [Benchmark]
+            public long ArrayWithoutPool()
+            {
+                int upperBound = arrayOfStrings.Length;
+                ProductStruct[] data = new ProductStruct[upperBound];
+                long result = 0;
+                for (int i = 0; i < upperBound; ++i)
                 {
-                    Id = i + 100,
-                    Name = arrayOfStrings[i],
-                    ManufacturerId = i + 10
-                };
+                    data[i] = new ProductStruct()
+                    {
+                        Id = i + 100,
+                        Name = arrayOfStrings[i],
+                        ManufacturerId = i + 10
+                    };
+                }
+                result = ProcessData(ref data);
+                return result;
             }
-            result = ProcessData(ref data);
-            return result;
-        }
-        [Benchmark]
-        public long ArrayUsingPool()
-        {
-            int upperBound = arrayOfStrings.Length;
-            ProductStruct[] data = ArrayPool<ProductStruct>.Shared.Rent(upperBound);
-            long result = 0;
-            for (int i = 0; i < upperBound; ++i)
+            [Benchmark]
+            public long ArrayUsingPool()
             {
-                data[i] = new ProductStruct()
+                int upperBound = arrayOfStrings.Length;
+                ProductStruct[] data = ArrayPool<ProductStruct>.Shared.Rent(upperBound);
+                long result = 0;
+                for (int i = 0; i < upperBound; ++i)
                 {
-                    Id = i + 100,
-                    Name = arrayOfStrings[i],
-                    ManufacturerId = i + 10
-                };
+                    data[i] = new ProductStruct()
+                    {
+                        Id = i + 100,
+                        Name = arrayOfStrings[i],
+                        ManufacturerId = i + 10
+                    };
+                }
+                result = ProcessData(ref data);
+                ArrayPool<ProductStruct>.Shared.Return(data);
+                return result;
             }
-            result = ProcessData(ref data);
-            ArrayPool<ProductStruct>.Shared.Return(data);
-            return result;
+
+            private long ProcessData(ref ProductStruct[] data)
+            {
+                long result = 0;
+                for (int i = 0; i < data.Length; i++)
+                {
+                    result += data[i].Id;
+                }
+                return result;
+            }
+        }
+        //https://adamsitnik.com/Array-Pool/
+        [MemoryDiagnoser]
+        [Config(typeof(DontForceGcCollectionsConfig))] // we don't want to interfere with GC, we want to include it's impact
+        public class ArrayPoolOfBytes
+        {
+            [Params((int)1E+2, // 100 bytes
+                (int)1E+3, // 1 000 bytes = 1 KB
+                (int)1E+4, // 10 000 bytes = 10 KB
+                (int)1E+5, // 100 000 bytes = 100 KB
+                (int)1E+6, // 1 000 000 bytes = 1 MB
+                (int)1E+7)] // 10 000 000 bytes = 10 MB
+            public int SizeInBytes { get; set; }
+
+            private ArrayPool<byte> sizeAwarePool;
+
+            [GlobalSetup]
+            public void GlobalSetup()
+                => sizeAwarePool = ArrayPool<byte>.Create(SizeInBytes + 1, 10); // let's create the pool that knows the real max size
+
+            [Benchmark]
+            public void Allocate()
+                => DeadCodeEliminationHelper.KeepAliveWithoutBoxing(new byte[SizeInBytes]);
+
+            [Benchmark]
+            public void RentAndReturn_Shared()
+            {
+                var pool = ArrayPool<byte>.Shared;
+                byte[] array = pool.Rent(SizeInBytes);
+                pool.Return(array);
+            }
+
+            [Benchmark]
+            public void RentAndReturn_Aware()
+            {
+                var pool = sizeAwarePool;
+                byte[] array = pool.Rent(SizeInBytes);
+                pool.Return(array);
+            }
         }
 
-        private long ProcessData(ref ProductStruct[] data)
+
+    }
+    public class DontForceGcCollectionsConfig : ManualConfig
+    {
+        public DontForceGcCollectionsConfig()
         {
-            long result = 0;
-            for (int i = 0; i < data.Length; i++)
-            {
-                result += data[i].Id;
-            }
-            return result;
+            AddJob(Job.Default
+                .WithGcMode(new GcMode()
+                {
+                    Force = false // tell BenchmarkDotNet not to force GC collections after every iteration
+                }));
         }
     }
 }
--- samples/BenchmarkNetPlayground/ScratchPad/ScratchPadOne.cs
+++ samples/BenchmarkNetPlayground/ScratchPad/ScratchPadOne.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.Extensions.Primitives;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BenchmarkNetPlayground.ScratchPad
+﻿namespace BenchmarkNetPlayground.ScratchPad
 {
+    using BenchmarkDotNet.Running;
+
+    using BenchmarkNetPlayground.Optimizations.Uncommon;
+    using BenchmarkNetPlayground.Services;
+
+    using Configs;
+
+    using Microsoft.Extensions.Primitives;
+
+    using Optimizations;
+    using Optimizations.Collections;
+    using Optimizations.Loops;
+    using Optimizations.Memory;
+    using Optimizations.Pooling;
+
+    using ScratchPad;
+
+    using System;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using static BenchmarkNetPlayground.Optimizations.Collections.Lists;
+
     public class ScratchPadOne
     {
 
@@ -21,6 +40,95 @@ namespace BenchmarkNetPlayground.ScratchPad
         }
 
 
+        public static void BenchmarkSnippets()
+        {
+            //ArrayPooling
+            //RunForLoopBenchmarks();
+
+
+            //BenchmarkRunner.Run<ArrayPooling>
+            //    (ManualConfigurations.GetManualConfig_Default_Job());
+            //BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+
+            #region "String Search"
+
+            //   BenchmarkRunner.Run<Optimizations.Strings.StringSearch.TrieVsDictionary>
+            //(ManualConfigurations.GetManualConfig_Default_Job());
+
+            #endregion "String Search"
+
+            #region "Value vs Ref Types"
+
+            //BenchmarkRunner.Run<RefAllocation>();
+
+            #endregion "Value vs Ref Types"
+
+
+
+            #region "For loops"
+
+
+
+
+            #endregion "For loops"
+
+
+            #region "Dictionary Benchmarks"
+
+            //BenchmarkRunner.Run<DictionaryConcreteVsInterface>(
+            //  ManualConfigurations.GetManualConfig_Net472_CoreRt31_64());
+
+            //BenchmarkRunner.Run<Optimizations.DictionaryWithOrWithoutSupplyingInitialCount>(
+            //    ManualConfigurations.GetManualConfig_Default_Job());
+
+
+            #endregion "Dictionary Benchmarks"
+
+            // Memory Benchmarks
+            //BenchmarkRunner.Run<IntroNativeMemory>();
+            //var t1 = new StringAllocations.SingleStringVsChunks();
+            //var singleString = t1.GetSingleString();
+            //var array = t1.GetListOfStrings();
+            //Console.WriteLine(array[0]);
+
+            //Console.WriteLine(array[1]);
+            //BenchmarkRunner.Run<StringAllocations.SingleStringVsChunks>();
+            // Compiler Inlining Benchmarks
+            //BenchmarkRunner.Run<Optimizations.Inlining>();
+
+
+
+            #region "Sorting"
+
+            //BenchmarkRunner.Run<SortingArraysUsingSpan.DoubleSorting>(ManualConfigurations.GetManualConfig_Net472_CoreRt31_DotNet50_64());
+            //BenchmarkRunner.Run<SortingArraysUsingSpan.Int32Sorting>(ManualConfigurations.GetManualConfig_Net472_CoreRt31_DotNet50_64());
+            //BenchmarkRunner.Run<SortingArraysUsingSpan.StringSorting>(ManualConfigurations.GetManualConfig_Net472_CoreRt31_DotNet50_64());
+
+            #endregion "Sorting"
+        }
+
+        static void RunForLoopBenchmarks()
+        {
+            //Runs benchmarks against array of ref types
+
+            //BenchmarkRunner.Run<Optimizations.Loops.ForLoops.ForVsForEach>
+            //   (ManualConfigurations.GetManualConfig_Default_Job());
+
+            BenchmarkRunner.Run<Lists.ListWithOrWithoutSupplyingInitialCount>
+                (ManualConfigurations.GetManualConfig_Default_Job());
+
+
+            ////Runs benchmarks against aray of value types with sequential layout
+            //BenchmarkRunner.Run<Optimizations.Loops.ForLoops.ForVsForEachUsingStructLayoutSequential>
+            //    (ManualConfigurations.GetManualConfig_Default_Job());
+
+
+            ////Runs benchmarks against aray of value types vs ref types
+
+            //BenchmarkRunner.Run<Optimizations.Loops.ForLoops.ForVsForEachClassVsSequentialStruct>
+            //    (ManualConfigurations.GetManualConfig_Default_Job());
+        }
 
         public void TestLargeStringArray()
         {
